59c4dff12ca6d7edd54d5b1ec4c6dcd3f004b8a3
Sunanda123-png <sunanda02@gmail.com>
2021-10-20 23:59:26 +0530

    Add: Removing the word

--- src/com/bridgelabz/FindFrequency.java
+++ src/com/bridgelabz/FindFrequency.java
@@ -1,11 +1,12 @@
 package com.bridgelabz;
 import java.util.LinkedHashMap;
+import java.util.TooManyListenersException;
 
 public class FindFrequency {
     public static void main(String[] args) {
         System.out.println("Welcome to Hash Table program");
         LinkedHashMap<String, Integer> frequency = new LinkedHashMap<>();
-        String message = "To be or not to be";
+        String message = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         String[] messageArray = message.toLowerCase().split(" ");
         for (String word : messageArray) {
@@ -16,6 +17,8 @@ public class FindFrequency {
                 frequency.put(processed, 1);
             }
         }
+        System.out.println(frequency);
+        frequency.remove("paranoid");
         System.out.println(frequency);
     }
 }
--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,81 +1,62 @@
 package com.bridgelabz;
 
-import java.util.ArrayList;
-
 public class LinkedList<K,V> {
     MyMapNode head;
     MyMapNode tail;
-    private final int numOfBuckets;
-    ArrayList<MyMapNode<K, V>> myBucketArray;
-
-    public LinkedList() {
-        this.numOfBuckets = 20;
-        this.myBucketArray = new ArrayList<>();
-        for (int i = 0; i < numOfBuckets; i++)
-            this.myBucketArray.add(null);
-    }
 
     public void add(K key, V value) {
-        int index = this.getBucketIndex(key);
-        MyMapNode<K, V> myNewNode = this.myBucketArray.get(index);
-        if (myNewNode == null) {
-            myNewNode = new MyMapNode<>(key, value);
-            this.myBucketArray.set(index, myNewNode);
-        }
-        myNewNode = (MyMapNode<K, V>) searchNode(key);
+        MyMapNode<K, V> myNewNode = (MyMapNode<K, V>) searchNode(key);
         if (myNewNode == null) {
             myNewNode = new MyMapNode<>(key, value);
             this.append(myNewNode);
-        } else {
+        } else
+        {
             myNewNode.setValue(value);
         }
     }
 
-    private int getBucketIndex(K word) {
-        int hashCode = Math.abs(word.hashCode());
-        int index = hashCode % numOfBuckets;
-        return index;
-    }
-
-    public void append(MyMapNode<K, V> myNewNode) {
+    public void append(MyMapNode<K, V> myNode) {
         if (this.head == null)
-            this.head = myNewNode;
+            this.head = myNode;
         if (this.tail == null)
-            this.tail = myNewNode;
+            this.tail = myNode;
         else {
-            this.tail.setNext(myNewNode);
-            this.tail = myNewNode;
+            this.tail.setNext(myNode);
+            this.tail = myNode;
         }
     }
 
     public MyMapNode<K, V> searchNode(K data) {
-        MyMapNode<K, V> currentNode = head;
+        MyMapNode<K, V> presentNode = head;
         int position = 0;
-        while (currentNode != null) {
+        while (presentNode != null) {
             position++;
-            if (currentNode.getKey().equals(data)) {
-                return currentNode;
+            if (presentNode.getKey().equals(data)) {
+                return presentNode;
             }
-            currentNode = currentNode.getNext();
+            presentNode = presentNode.getNext();
         }
-        return currentNode;
+        return presentNode;
     }
 
     public V get(K word) {
-        int index = this.getBucketIndex(word);
-        MyMapNode<K, V> myNewNode = this.myBucketArray.get(index);
-        if (this.myBucketArray.get(index) == null)
-            return null;
-        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) searchNode(word);
+        MyMapNode<K, V> myMapNode = searchNode(word);
         return (myMapNode == null) ? null : myMapNode.getValue();
     }
+    public void remove(K word) {
+
+        MyMapNode presentNode = head;
+        MyMapNode previousNode = null;
+        if (presentNode != null) {
+            previousNode.next = presentNode.next;
+        }
+        if (presentNode == null)
+            System.out.println("Word not found!");
+    }
+
 
     @Override
     public String toString() {
         return "MyLinkedListNodes{" + head + "}";
-    }
-
-    public void printNodes() {
-        System.out.println("My nodes: " + head);
     }
 }
